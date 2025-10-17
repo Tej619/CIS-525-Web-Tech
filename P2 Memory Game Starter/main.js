cc46c37ba2064bc456829a1de19de5b8080487c7
Tejas Vaity <tejas.vaity3@gmail.com>
2025-10-16 21:37:45 -0400

    Memory Game Project

---
+++ P2 Memory Game Starter/main.js
@@ -0,0 +1,25 @@
+"use strict";
+const getElement = selector => document.querySelector(selector);
+
+// image constants 
+const backImgSrc = "images/back.png";
+const blankImgSrc = "images/blank.png";
+const cardImgSrcStart = "images/card_";
+
+document.addEventListener("DOMContentLoaded", () => {
+    // display cards and player info
+    
+
+    // load settings data
+    
+ 
+	// add click event handler for each card link
+
+
+    // add click event handler for each tab link button
+    
+
+    // add click event handler for Save Settings button
+    
+    
+}); 
